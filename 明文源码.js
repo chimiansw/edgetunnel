cd8033dc5d5906a5d8f9efa7136f3ee811856dd3
chimiansw <52277562+chimiansw@users.noreply.github.com>
2025-04-25 10:27:51 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '1be05eba-9fdb-4a81-b680-c154ab517492';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
